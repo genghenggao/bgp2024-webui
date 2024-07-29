cbda6e41ed30330bfb51e9c501da1bb103292c03
genghenggao <genghenggao@outlook.com>
2024-07-29 10:03:46 +0800

    commit bgp2024webui

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+/*
+ * @Description: henggao_note
+ * @version: v1.0.0
+ * @Date: 2024-07-20 11:05:26
+ * @LastEditors: henggao
+ * @LastEditTime: 2024-07-29 09:54:27
+ */
 module.exports = {
-  publicPath: './'
+  publicPath: './',
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'BGP2024 WEB'
+        return args
+      })
+  }
 }
